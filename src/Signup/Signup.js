a9311279f1460dee70d935ea1504e670ebad9d09
Kyle Hall <khall@pillartechnology.com>
2019-04-23 13:53:13 -0500

    getting a form working with a class-based component

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,33 +1,98 @@
 import React from 'react'
 import 'nes.css/css/nes.css'
 
-export const Signup = props => (
-  <form style={{ ...styles.form, ...styles.typ }}>
-    <fieldset style={styles.fieldset}>
-      <legend style={styles.legend}>Name</legend>
-      <label for='firstName'>First Name</label>
-      <input className='nes-input' type='text' name='firstName' id='firstName' required />
-      <label for='lastName'>Last Name</label>
-      <input className='nes-input' type='text' name='lastName' id='lastName' required />
-    </fieldset>
-    <fieldset style={styles.fieldset}>
-      <legend style={styles.legend}>Contact</legend>
-      <label for='emailInput'>Email Address</label>
-      <input className='nes-input' type='email' name='email' id='emailInput' required />
-    </fieldset>
-    <fieldset style={styles.fieldset}>
-      <legend style={styles.legend}>Password</legend>
-      <label for='passwordInput'>Password</label>
-      <input className='nes-input' type='password' name='password1' id='passwordInput' />
+export class Signup extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordConfirm: ''
+    }
 
-      <label for='passwordConfirm'>Re-enter Password</label>
-      <input className='nes-input' type='password' name='passwordConfirm' id='passwordConfirm' />
-    </fieldset>
-    <button className='nes-btn' style={styles.typ} type='submit'>
-      Sign Up
-    </button>
-  </form>
-)
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  render() {
+    const { firstName, lastName, email, password, confirmPassword } = this.state
+
+    return (
+      <form style={{ ...styles.form, ...styles.typ }}>
+        <fieldset style={styles.fieldset}>
+          <legend style={styles.legend}>Name</legend>
+          <label for='firstName'>First Name</label>
+          <input
+            className='nes-input'
+            type='text'
+            value={firstName}
+            name='firstName'
+            id='firstName'
+            onChange={this.handleInputChange}
+            required
+          />
+          <label for='lastName'>Last Name</label>
+          <input
+            className='nes-input'
+            type='text'
+            value={lastName}
+            name='lastName'
+            id='lastName'
+            onChange={this.handleInputChange}
+            required
+          />
+        </fieldset>
+        <fieldset style={styles.fieldset}>
+          <legend style={styles.legend}>Contact</legend>
+          <label for='emailInput'>Email Address</label>
+          <input
+            className='nes-input'
+            type='email'
+            value={email}
+            name='email'
+            id='emailInput'
+            onChange={this.handleInputChange}
+            required
+          />
+        </fieldset>
+        <fieldset style={styles.fieldset}>
+          <legend style={styles.legend}>Password</legend>
+          <label for='passwordInput'>Password</label>
+          <input
+            className='nes-input'
+            type='password'
+            value={password}
+            name='password'
+            onChange={this.handleInputChange}
+            id='passwordInput'
+          />
+
+          <label for='passwordConfirm'>Re-enter Password</label>
+          <input
+            className='nes-input'
+            type='password'
+            value={confirmPassword}
+            name='passwordConfirm'
+            onChange={this.handleInputChange}
+            id='passwordConfirm'
+          />
+        </fieldset>
+        {/* I want the button to actually submit to something - maybe I could set up a lambda to do something with it? */}
+        <button className='nes-btn' style={styles.typ} type='submit'>
+          Sign Up
+        </button>
+      </form>
+    )
+  }
+
+  handleInputChange(event) {
+    const {
+      target: { name, value }
+    } = event
+    this.setState({ [name]: value })
+  }
+}
 
 const styles = {
   form: {
@@ -39,7 +104,7 @@ const styles = {
     backgroundColor: '#A4E4FC'
   },
   legend: {
-    backgroundColor: '#000',
+    backgroundColor: '#111',
     color: '#fff',
     padding: '3px 6px',
     borderRadius: 3
